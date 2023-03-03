f59ba18ee74d15cdc1a1dac4904386f5d1e4dc31
mooney0129 <mooneydev9001@gmail.comgit commit -m>
2023-03-02 20:04:30 -0500

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+# 🏗 scaffold-eth | 🏰 BuidlGuidl
+
+## 🚩 Challenge 1: 🥩 Decentralized Staking App
+
+## Moved to [challenges](https://github.com/scaffold-eth/scaffold-eth-challenges) repo
---
+++ packages/hardhat/contracts/Staker.sol
@@ -0,0 +1,36 @@
+pragma solidity >=0.6.0 <0.8.4;
+
+import "hardhat/console.sol";
+import "./ExampleExternalContract.sol";
+
+contract Staker {
+
+  ExampleExternalContract public exampleExternalContract;
+
+  mapping(address => uint256) public balances;
+  uint256 public constant threshold = 1 ether;
+
+  event Stake(address indexed sender, uint256 amount);
+
+  constructor(address exampleExternalContractAddress) public {
+      exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
+  }
+
+  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
+  //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+
+
+  // After some `deadline` allow anyone to call an `execute()` function
+  //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+
+
+  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+
+
+  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+
+
+  // Add the `receive()` special function that receives eth and calls stake()
+
+
+}
