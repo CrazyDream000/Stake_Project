fe50e764f0dc51d2553f4e4e516384f94c4ea63a
mooney0129 <mooneydev9001@gmail.comgit commit -m>
2023-03-02 21:02:33 -0500

    update

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -10,12 +10,41 @@ contract Staker {
   mapping(address => uint256) public balances;
   uint256 public constant threshold = 1 ether;
 
+
+  uint256 public deadline = block.timestamp + 30 seconds;
   event Stake(address indexed sender, uint256 amount);
+
+
+  modifier deadlineReached( bool requireReached ) {
+    uint256 timeRemaining = timeLeft();
+    if( requireReached ) {
+      require(timeRemaining == 0, "Deadline is not reached yet");
+    } else {
+      require(timeRemaining > 0, "Deadline is already reached");
+    }
+    _;
+  }
+
+  /**
+  * @notice Modifier that require the external contract to not be completed
+  */
+  modifier stakeNotCompleted() {
+    bool completed = exampleExternalContract.completed();
+    require(!completed, "staking process already completed");
+    _;
+  }
 
   constructor(address exampleExternalContractAddress) public {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
+  function stake() public payable deadlineReached(false) stakeNotCompleted {
+    // update the user's balance
+    balances[msg.sender] += msg.value;
+    
+    // emit the event to notify the blockchain that we have correctly Staked some fund for the user
+    emit Stake(msg.sender, msg.value);
+  }
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
 
@@ -23,7 +52,17 @@ contract Staker {
   // After some `deadline` allow anyone to call an `execute()` function
   //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
 
+  function execute() public stakeNotCompleted deadlineReached(false) {
+    uint256 contractBalance = address(this).balance;
 
+    // check the contract has enough ETH to reach the treshold
+    require(contractBalance >= threshold, "Threshold not reached");
+
+    // Execute the external contract, transfer all the balance to the contract
+    // (bool sent, bytes memory data) = exampleExternalContract.complete{value: contractBalance}();
+    (bool sent,) = address(exampleExternalContract).call{value: contractBalance}(abi.encodeWithSignature("complete()"));
+    require(sent, "exampleExternalContract.complete failed");
+  }
   // if the `threshold` was not met, allow everyone to call a `withdraw()` function
 
 
